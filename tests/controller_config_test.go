9a69e39c21e78abf2dd14fed665744d4d21bc287
Chris Wilson <hey@chris-wilson.me>
2024-02-12 19:41:34 +0000

    Move etcd config into its own role

--- tests/controller_config_test.go
+++ tests/controller_config_test.go
@@ -18,27 +18,6 @@ func TestControllerConfig(t *testing.T) {
 			t.Errorf("wanted %v, got %v", tt.vmName, hostname)
 		}
 
-		pemFiles := []string{
-			"ca.pem",
-			"kubernetes-key.pem",
-			"kubernetes.pem",
-			"service-account-key.pem",
-			"service-account.pem",
-			"admin.kubeconfig",
-			"kube-controller-manager.kubeconfig",
-			"kube-scheduler.kubeconfig",
-			"encryption-config.yaml",
-		}
-
-		for _, file := range pemFiles {
-			t.Run(tt.vmName+" has "+file, func(t *testing.T) {
-				hasFile := sshclient.HasFile(file)
-				if !hasFile {
-					t.Errorf("%v does not have %v", tt.vmName, file)
-				}
-			})
-		}
-
 		etcdFiles := []string{
 			"/etc/etcd/ca.pem",
 			"/etc/etcd/kubernetes-key.pem",
