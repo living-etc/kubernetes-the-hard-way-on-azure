fd72c1963af51556a935da03828fbf559738c7c0
Chris Wilson <hey@chris-wilson.me>
2024-02-16 02:39:51 +0000

    Remove redundant worker tests

--- tests/worker_config_test.go
+++ tests/worker_config_test.go
@@ -11,23 +11,6 @@ func TestWorkerConfig(t *testing.T) {
 		sshclient, err := ssh.NewSshClient(privateKey, tt.hostname)
 		check(err, "Unable to get VM from name")
 
-		pemFiles := []string{
-			"ca.pem",
-			tt.vmName + "-key.pem",
-			tt.vmName + ".pem",
-			tt.vmName + ".kubeconfig",
-			"kube-proxy.kubeconfig",
-		}
-
-		for _, file := range pemFiles {
-			t.Run(tt.vmName+" has "+file, func(t *testing.T) {
-				hasFile := sshclient.HasFile(file)
-				if !hasFile {
-					t.Errorf("%v does not have %v", tt.vmName, file)
-				}
-			})
-		}
-
 		binaries := []string{
 			"/usr/local/bin/kube-proxy",
 			"/usr/local/bin/kubelet",
