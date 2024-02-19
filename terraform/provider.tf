f39b62ecfff5b11ddcd31e06a75645957c2c89ed
Chris Wilson <hey@chris-wilson.me>
2024-02-19 20:12:41 +0000

    Fix terraform make tasks

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  config_path = "../kubeconfig/admin.kubeconfig"
+  config_path = "kubeconfig/admin.kubeconfig"
   host        = "https://kthw-cw.uksouth.cloudapp.azure.com:6443"
   insecure    = true
 }
