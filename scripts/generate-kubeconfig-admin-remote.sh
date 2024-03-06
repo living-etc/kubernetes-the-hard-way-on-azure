c780fb59f3ca90c119a827ef68ea354542bb5c7e
Chris Wilson <hey@chris-wilson.me>
2024-03-06 13:31:12 +0000

    Scope context to Azure
    This is to differentiate it from the AWS cluster, which might be running
    at the same time as this one.

--- scripts/generate-kubeconfig-admin-remote.sh
+++ scripts/generate-kubeconfig-admin-remote.sh
@@ -14,17 +14,17 @@ KUBERNETES_PUBLIC_ADDRESS=$(
     --output tsv
 )
 
-kubectl config set-cluster kubernetes-the-hard-way \
+kubectl config set-cluster kthw-azure \
   --certificate-authority=${TLS_PATH}/ca.pem \
   --embed-certs=true \
   --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
 
-kubectl config set-credentials admin \
+kubectl config set-credentials admin-azure \
   --client-certificate=${TLS_PATH}/admin.pem \
   --client-key=${TLS_PATH}/admin-key.pem \
 
-kubectl config set-context kubernetes-the-hard-way \
-  --cluster=kubernetes-the-hard-way \
-  --user=admin
+kubectl config set-context kthw-azure \
+  --cluster=kthw-azure \
+  --user=admin-azure
 
-kubectl config use-context kubernetes-the-hard-way
+kubectl config use-context kthw-azure
